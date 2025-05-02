7cff7caf0d5cae6943648a6ced52da8da19f234e
Farid Mustafayev <98546766+rafael4206@users.noreply.github.com>
2025-05-02 08:33:33 +0400

    Add files via upload

---
+++ proyekt.java
@@ -0,0 +1,8 @@
+
+public class proyekt {
+    public static void main(String[] args) {
+        Mekteb qrup1 = new Mekteb("Ferid", "Aynur");
+        System.out.println(qrup1.muellim);
+        qrup1.derse_basla();
+
+    }}
