0088b327dc23a3dc5c3744434bd53d04cc7e175a
Farid Mustafayev <98546766+rafael4206@users.noreply.github.com>
2025-05-02 08:35:05 +0400

    Delete proyekt.java

--- proyekt.java
+++
@@ -1,8 +0,0 @@
-
-public class proyekt {
-    public static void main(String[] args) {
-        Mekteb qrup1 = new Mekteb("Ferid", "Aynur");
-        System.out.println(qrup1.muellim);
-        qrup1.derse_basla();
-
-    }}
